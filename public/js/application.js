22191df49bd1f291afa65adec30f9e6fd4817a53
Apprentice <apprentice@devbootcamp.com>
2013-08-05 08:21:51 -0700

    fixed code

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-
+  $('#get_color').click(function(e){
+    e.preventDefault();
+    $.ajax({
+      type: 'post',
+      url: '/color',
+      dataType: 'json'
+    }).done(function(data){
+      $('#color_me li').eq(data.cell).css("background-color",data.color);
+    });
+  });
 });
